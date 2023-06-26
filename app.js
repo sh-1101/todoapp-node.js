065e98e0290ebd20b408c2eb66462276353bc556
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-06-26 21:11:37 +0900

    feat: Node.jsとMongoDBを接続

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 const taskRoute = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
 const PORT = 5000;
 
+//ルーティング設定
 app.use("/api/v1/tasks", taskRoute);
 
-app.listen(PORT, console.log("サーバーが起動しました"));
+//データベースと接続
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, console.log("サーバーが起動しました"));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
