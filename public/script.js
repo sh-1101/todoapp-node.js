2f6f83ac31067bd540a395984861e89eb6f40591
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-06-28 22:31:12 +0900

    feat: axiosでNode.jsサーバーと連携してタスクを全て取得

---
+++ public/script.js
@@ -0,0 +1,35 @@
+const tasksDOM = document.querySelector(".tasks");
+
+// /api/v1/tasksからタスクを読み込む
+const showTasks = async () => {
+  try {
+    //自作のAPIを叩く
+    const { data: tasks } = await axios.get("/api/v1/tasks");
+
+    //タスクを出力
+    const allTasks = tasks
+      .map((task) => {
+        const { completed, _id, name } = task;
+
+        return `<div class="single-task">
+      <h5>
+        <span><i class="far fa-check-circle"></i></span>${name}
+      </h5>
+      <div class="task-links">
+        <a href="#" class="edit-link">
+          <i class="fas fa-edit"></i>
+        </a>
+        <button type="button" class="delete-btn">
+          <i class="fas fa-trash"></i>
+        </button>
+      </div>
+    </div>`;
+      })
+      .join("");
+    tasksDOM.innerHTML = allTasks;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+showTasks();
